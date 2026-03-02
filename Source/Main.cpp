9dd7b6f922a40007be50df370370a0491877a57c
flarkflarkflark <flarkflarkflark@gmail.com>
2026-03-02 19:16:12 +0100

    feat: add AppImage for Linux and DMG for macOS portable releases

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -13,7 +13,7 @@ public:
     VinylRestorationApplication() {}
 
     const juce::String getApplicationName() override       { return "Vinyl Restoration Suite"; }
-    const juce::String getApplicationVersion() override    { return "1.6.23"; }
+    const juce::String getApplicationVersion() override    { return "1.6.31"; }
     bool moreThanOneInstanceAllowed() override             { return true; }
 
     //==============================================================================
